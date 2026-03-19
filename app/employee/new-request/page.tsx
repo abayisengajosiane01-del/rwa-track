bcad0386e4313e1586120d8bb09ffbe1dae84cef
Bananayosostene <sbananayo98@gmail.com>
2026-03-19 16:44:11 +0200

    fix: add use client to new-request page wrapper for ssr:false support

--- app/employee/new-request/page.tsx
+++ app/employee/new-request/page.tsx
@@ -1,15 +1,10 @@
+"use client";
+
 import dynamicImport from "next/dynamic";
 
 const EmployeeNewRequestPage = dynamicImport(
   () => import("./new-request-client"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 flex items-center justify-center">
-        <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
-      </div>
-    ),
-  },
+  { ssr: false },
 );
 
 export default EmployeeNewRequestPage;
